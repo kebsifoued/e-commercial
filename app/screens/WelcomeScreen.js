4672e1d7b2132ac70661b613e648180fdb770572
kebsifoued <kebsifoued.fk@gmail.com>
2024-08-06 12:42:11 -0400

    edit in welcome screen page

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
@@ -7,14 +7,20 @@ function WelcomeScreen(props) {
         style={styles.background}
           source={require("../assets/welcomeScreen.png")}
         >
-            {/* <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View> */}
+            <View style={styles.logoContainer}>
+               <Image style={styles.logo} source={require("../assets/logo.png")}></Image>
+               <Text>Foued MarketPlace</Text>
+            </View>
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
         </ImageBackground>
     );
 }
 const styles = StyleSheet.create({
     background:{
-        flex:1
+        flex:1,
+        justifyContent:"flex-end",
+        alignItems:"center",
     },
     loginButton:{
         width:"100%",
@@ -25,6 +31,15 @@ const styles = StyleSheet.create({
         width:"100%",
         height:70,
         backgroundColor:"#4ecdc4"
+    },
+    logo:{
+        width:100,
+        height:100,
+    },
+    logoContainer:{
+        position:"absolute",
+        top:70,
+        alignItems:"center"
     }
 })
 
